051665c01128bea3f651957482931fea535d68a7
Michiel <163190291+FunTimeMicheal@users.noreply.github.com>
2025-04-03 21:36:07 +0200

    DL0.1

---
+++ IWAApplication/src/database/DataLogger.php
@@ -0,0 +1,30 @@
+<?php
+
+echo "Datalogger gestart...\n";
+while(true)
+{
+    if ($buffer) {
+        
+        $segments = explode(";", trim($buffer));
+        $parsedData = [];
+        
+        foreach ($segments as $segment) {
+            list($key, $value) = explode(":", $segment);
+            $parsedData[trim($key)] = trim($value);
+        }
+        
+        file_put_contents($dataFile, json_encode($parsedData, JSON_PRETTY_PRINT));
+        
+        echo "Data opgeslagen: " . json_encode($parsedData) . "\n";
+        
+        $temperature = $parsedData['TEMP'] ?? null;
+        $windSpeed = $parsedData['WIND'] ?? null;
+        $humidity = $parsedData['HUM'] ?? null;
+        
+        echo "Data opgeslagen in database.\n";
+    }
+    
+    sleep(1); 
+}
+
+?>
---
+++ IWAGenerator/src/Main.java
@@ -0,0 +1,13 @@
+import app.Application;
+
+// first version of the Main
+class Main {
+  // constructor of the Main function
+  public Main() {
+
+  }
+
+  public static void main(String[] args) {
+    Application.getInstance();
+  }
+}
