0761db233375ca57c2849139b4570d4329d226a2
ricardobos <68953190+Boskovo@users.noreply.github.com>
2025-04-12 20:44:31 +0200

    removed a comment

--- IWAApplication/src/Views/stations.php
+++ IWAApplication/src/Views/stations.php
@@ -15,7 +15,6 @@
 
         try {
             const response = await fetch(url);
-            console.log(response)
             if(!response.ok) {
                 throw new Error(`Response status: ${response.status}`);
             }
