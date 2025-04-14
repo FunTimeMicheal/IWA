12ba6488bc72b121dcc01d83f58b7d53bc10df55
MichielVanDerHeide <163190291+FunTimeMicheal@users.noreply.github.com>
2025-04-14 21:56:16 +0200

    v1

--- IWAApplication/src/Routes/Api/ApiController.php
+++ IWAApplication/src/Routes/Api/ApiController.php
@@ -13,8 +13,22 @@ class ApiController {
     $group->group("/stations", new StationsController());
     $group->group("/users", new UsersController());
 
-    $group->post("/ingest", function (Request $request, Response $response) {
-      //
+    $group->post("/ingest", function (Request $request, Response $response, array $args) {
+      $data = $request->getParsedBody();
+        if (!$data) {
+            $response->getBody()->write(json_encode([
+                'status' => 'error',
+                'message' => 'Ongeldige JSON'
+            ]));
+            return $response->withHeader('Content-Type', 'application/json')->withStatus(400);
+          }
+        file_put_contents(__DIR__ . '/../../../Logs/ingest_log.json', json_encode($data, JSON_PRETTY_PRINT));
+        $response->getBody()->write(json_encode([
+            'status' => 'success',
+            'message' => 'Data ontvangen',
+            'received' => $data
+          ]));
+        return $response->withHeader('Content-Type', 'application/json')->withStatus(200);
     });
   }
 }
