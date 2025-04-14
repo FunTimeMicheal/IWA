39ad6e8632d2af36a36f58235201bb98db9e1541
ricardobos <68953190+Boskovo@users.noreply.github.com>
2025-04-14 22:22:51 +0200

    removed general getData function

--- IWAApplication/public/js/apicalls.js
+++ IWAApplication/public/js/apicalls.js
@@ -1,35 +1,3 @@
-async function getData(url) {
-    try {
-        const response = await fetch(url);
-        if(!response.ok) {
-            throw new Error(`Response status: ${response.status}`);
-        }
-
-        const json = await response.json().catch(() => {});
-        console.log(json)
-
-        for (const item of json) {
-            const dataElement = Object.assign(document.createElement('div'), {
-                className: 'region',
-                innerHTML: /* html *//* html */`
-                <h2>Groningen</h2>
-                <div class="stations">
-                    <h4 class="station">0</h4>
-                    <h4 class="station error">1</h4>
-                    <h4 class="station warning">2</h4>
-                </div>
-              `,
-            });
-    
-            const parent = document.getElementById("stations");
-            parent.appendChild(dataElement);
-        }
-        
-
-    } catch (error) {
-        console.error(error);
-    }
-}
 
 async function postData(url) {
     try {
